29554c91fd3f2f59a26a8915bb83f6bc9f08a59a
Giovanbattista Gravina <gianba.gravina7@gmail.com>
2023-08-10 15:22:13 +0200

    consider also alternative formulation, not work

--- source/modNE.m
+++ source/modNE.m
@@ -1,9 +1,5 @@
 function u = modNE(dq,v,ddq,m,I,A,pc,g0)
 % Implementation of Modified Newton-Euler algorithm
-if ~exist('g0','var')
-     % no gravity acceleration parameter, default it to 0
-      g0 = 0;
-end
 
 n = size(dq,1);
 % check dimension consistency
@@ -28,11 +24,15 @@ u = zeros(n,1);
 
 % forward recursion
 for i = 2:n+1
-   w(:,i) = R(:,:,i-1)'*(w(:,i-1)+dq(i-1)*[0 0 1]');
-   wa(:,i) = R(:,:,i-1)'*(wa(:,i-1)+v(i-1)*[0 0 1]');
-   dw(:,i) = R(:,:,i-1)'*(dw(:,i-1)+ddq(i-1)*[0 0 1]'+cross(dq(i-1)*wa(:,i-1),[0 0 1]'));
-   a(:,i) = R(:,:,i-1)'*a(:,i-1)+cross(dw(:,i),p(:,i-1))+cross(w(:,i),cross(wa(:,i),p(:,i-1)));
-   ac(:,i) = a(:,i)+cross(dw(:,i),pc(:,i-1))+cross(w(:,i),cross(wa(:,i),pc(:,i-1)));
+    w(:,i) = R(:,:,i-1)'*(w(:,i-1)+dq(i-1)*[0 0 1]');
+    wa(:,i) = R(:,:,i-1)'*(wa(:,i-1)+v(i-1)*[0 0 1]');
+%   dw(:,i) = R(:,:,i-1)'*(dw(:,i-1)+ddq(i-1)*[0 0 1]'+cross(dq(i-1)*wa(:,i-1),[0 0 1]'));
+%   a(:,i) = R(:,:,i-1)'*a(:,i-1)+cross(dw(:,i),p(:,i-1))+cross(w(:,i),cross(wa(:,i),p(:,i-1)));
+%   ac(:,i) = a(:,i)+cross(dw(:,i),pc(:,i-1))+cross(w(:,i),cross(wa(:,i),pc(:,i-1)));
+    % alternative formulation
+    dw(:,i) = R(:,:,i-1)'*(dw(:,i-1)+ddq(i-1)*[0 0 1]'+cross(v(i-1)*w(:,i-1),[0 0 1]'));
+    a(:,i) = R(:,:,i-1)'*a(:,i-1)+cross(dw(:,i),p(:,i-1))+cross(wa(:,i),cross(w(:,i),p(:,i-1)));
+    ac(:,i) = a(:,i)+cross(dw(:,i),pc(:,i-1))+cross(wa(:,i),cross(w(:,i),pc(:,i-1)));
 end
 
 w = w(:,2:n+1);
@@ -43,8 +43,11 @@ R = cat(3,R,eye(3));
 
 % backward recursion
 for i = n:-1:1
+%     f(:,i) = R(:,:,i+1)*f(:,i+1)+m(i)*ac(:,i);
+%     t(:,i) = R(:,:,i+1)*t(:,i+1)+cross(R(:,:,i+1)*f(:,i+1),pc(:,i))-cross(f(:,i),p(:,i)+pc(:,i))+I(:,:,i)*dw(:,i)+cross(w(:,i),I(:,:,i)*wa(:,i));
+    % alternative formulation
     f(:,i) = R(:,:,i+1)*f(:,i+1)+m(i)*ac(:,i);
-    t(:,i) = R(:,:,i+1)*t(:,i+1)+cross(R(:,:,i+1)*f(:,i+1),pc(:,i))-cross(f(:,i),p(:,i)+pc(:,i))+I(:,:,i)*dw(:,i)+cross(w(:,i),I(:,:,i)*wa(:,i));
+    t(:,i) = R(:,:,i+1)*t(:,i+1)+cross(R(:,:,i+1)*f(:,i+1),pc(:,i))-cross(f(:,i),p(:,i)+pc(:,i))+I(:,:,i)*dw(:,i)+cross(wa(:,i),I(:,:,i)*w(:,i));
     % assuming all revolute joints
     u(i) = t(:,i)'*(R(:,:,i)'*[0 0 1]');
 end
