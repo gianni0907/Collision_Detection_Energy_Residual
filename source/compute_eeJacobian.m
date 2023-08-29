535a66ce25dd2cf34880693406cb67b0a06f2a54
Luca Nunziante <luca.nunziante1999@gmail.com>
2023-08-29 19:09:04 +0200

    fixing long simulation times

--- source/compute_eeJacobian.m
+++ source/compute_eeJacobian.m
@@ -1,16 +1,17 @@
 clear variables
 clc
-% script to generate functions to retrieve the linear part of the end
-% effector jacobian and its derivative
-syms q1 q2 q3 dq1 dq2 dq3 l1 l2 l3 real
+% script to generate functions to retrieve the position of the end-effector,
+% the linear part of the end effector jacobian and its derivative
+syms q1 q2 q3 dq1 dq2 dq3 real
 q = [q1 q2 q3]';
 dq = [dq1 dq2 dq3]';
 l = [0.5 0.5 0.4]';
-DHTable = [pi/2 0       l1    q1;
-           0    l2    0       q2;
-           0    l3    0       q3];
-[T,A] = DHMatrix_sym(DHTable);
+DHTable = [pi/2 0       l(1)    q1;
+           0    l(2)    0       q2;
+           0    l(3)    0       q3];
+[T,A] = DHMatrix(DHTable);
 pee = T(1:3,4);
+matlabFunction(pee,'File','get_pee.m','Vars',[q1 q2 q3]);
 Jee = simplify(jacobian(pee,q));
 matlabFunction(Jee,'File','get_Jee.m','Vars',[q1 q2 q3]);
 dJee = simplify(diff(Jee,q1)*dq1 + diff(Jee,q2)*dq2 + diff(Jee,q3)*dq3);
--- source/robot_motion.m
+++ source/robot_motion.m
@@ -1,5 +1,11 @@
-function robot_motion(robot,q,p0,p1,c,r_c,T,T2,t)
+function robot_motion(robot,q,p0,p1,p2,c,r_c,f_ext,T,T2,t,save_video)
 % showing the motion of the robot
+if save_video
+    v = VideoWriter("videos/robot.avi");
+    v.FrameRate=floor(size(t,2)/max(t));
+    v.Quality=100;
+    open(v)
+end
 
 figure
 show(robot,homeConfiguration(robot),'Frames','off');
@@ -14,7 +20,7 @@ plot3(p1(1),p1(2),p1(3),'.','MarkerSize',18);
 plot3(p2(1),p2(2),p2(3),'.','MarkerSize',18);
 plot3([p0(1),p1(1)],[p0(2),p1(2)],[p0(3),p1(3)],'LineWidth',1.2);
 plot3(c(1)+r_c*cos(2*pi/T2*t_2),c(2)*ones(size(t_2,2),1),c(3)-r_c*sin(2*pi/T2*t_2),'LineWidth',1.2)
-
+pause
 framesPerSecond = 20;
 r = rateControl(framesPerSecond);
 config = homeConfiguration(robot);
@@ -24,8 +30,17 @@ for i = 1:size(q,2)
     config(3).JointPosition = q(3,i);
     show(robot, config, 'PreservePlot', false, 'FastUpdate', 1,'Collisions','on');
     hold on
+    pee = get_pee(q(1,i),q(2,i),q(3,i));
+    if i>1
+        delete(h)
+    end
+    h=quiver3(pee(1),pee(2),pee(3),f_ext(1,i),f_ext(2,i),f_ext(3,i),'LineWidth',1,'Color',[0 0 1]);
     title('$t = '+compose("%.2f",t(i))+' s$','Interpreter','Latex');
     drawnow
+    if save_video
+        frame = getframe(gcf);
+        writeVideo(v,frame);
+    end
     waitfor(r);
 end
 end
